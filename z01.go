381e86a98cf62617c10d071ea863498704a46019
Xavier Petit <le@vieux.pro>
2018-12-09 22:39:31 +0100

    Modernize Putchar to PrintRune.
    1. char => Rune
    Because `char` is a C type, not a Go one (although it
    has `byte` for an ASCII character). `rune` is the prefered Go type
    for a single glyph.
    
    Also, when iterating on a string, it will decompose it with runes, not
    bytes.
    
    2. Put => Print
    Because Go already use the verb "Print" to output text on console.

--- z01.go
+++ z01.go
@@ -2,7 +2,7 @@ package z01
 
 import "os"
 
-// Putchar prints a single ASCII character (if 0 <= c <= 127)
-func Putchar(c byte) {
-	os.Stdout.Write([]byte{c})
+// PrintRune prints a single rune (Unicode code point)
+func PrintRune(r rune) {
+	os.Stdout.Write([]byte(string(r)))
 }
