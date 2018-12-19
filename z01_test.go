d8bf083debbd906474cd94b08a41961d62b8b7fe
Xavier Petit <le@vieux.pro>
2018-12-19 18:37:11 +0100

    Add test examples

---
+++ z01_test.go
@@ -0,0 +1,17 @@
+package z01
+
+func Example1() {
+	PrintRune('0')
+	// Output:
+	// 0
+}
+
+func Example2() {
+	PrintRune('0')
+	PrintRune('1')
+	PrintRune('\n')
+	PrintRune('❤')
+	// Output:
+	// 01
+	// ❤
+}
