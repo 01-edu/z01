de6a5fca8df0ef07edb459d9107a58b79cd5e34f
Xavier Petit <le@vieux.pro>
2018-12-29 16:52:58 +0000

    Make PrintRune faster & safer

--- z01.go
+++ z01.go
@@ -1,8 +1,20 @@
 package z01
 
-import "os"
+import (
+	"errors"
+	"os"
+	"unicode/utf8"
+)
 
-// PrintRune prints a single rune (Unicode code point)
-func PrintRune(r rune) {
-	os.Stdout.Write([]byte(string(r)))
+// PrintRune prints a single rune (Unicode code point) and returns any error
+// if the encoding or the writing fails.
+func PrintRune(r rune) error {
+	l := utf8.RuneLen(r)
+	if l == -1 {
+		return errors.New("The rune is not a valid value to encode in UTF-8")
+	}
+	p := make([]byte, l)
+	utf8.EncodeRune(p, r)
+	_, err := os.Stdout.Write(p)
+	return err
 }
