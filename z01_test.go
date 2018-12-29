0c8c58aa956296d32db591fe9a6c054b17fbf2f8
Xavier Petit <le@vieux.pro>
2018-12-29 16:53:42 +0000

    Switch package name to "_test", use dot import and add invalid rune test

--- z01_test.go
+++ z01_test.go
@@ -1,16 +1,14 @@
-package z01
+package z01_test
 
-func Example1() {
-	PrintRune('0')
-	// Output:
-	// 0
-}
+import . "."
 
-func Example2() {
+func ExamplePrintRune() {
 	PrintRune('0')
 	PrintRune('1')
 	PrintRune('\n')
 	PrintRune('❤')
+	invalidRune := rune(-1)
+	PrintRune(invalidRune)
 	// Output:
 	// 01
 	// ❤
