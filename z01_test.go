8798cfa09541b7c5c656d66b52c528f7a64129a7
Xavier Petit <le@vieux.pro>
2019-03-04 00:56:05 +0000

    Improve Z01 test

--- z01_test.go
+++ z01_test.go
@@ -8,7 +8,10 @@ func ExamplePrintRune() {
 	z01.PrintRune('\n')
 	z01.PrintRune('♥')
 	invalidRune := rune(-1)
-	z01.PrintRune(invalidRune)
+	err := z01.PrintRune(invalidRune)
+	if err == nil {
+		panic("z01.PrintRune should fail with an invalid rune")
+	}
 	// Output:
 	// 01
 	// ♥
